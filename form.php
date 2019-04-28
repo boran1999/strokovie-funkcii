74afae8684633414ce06e788613dae60c1140f15
Anton Borisov <32795987+boran1999@users.noreply.github.com>
2019-04-28 12:00:49 +0800

    Create form.php

---
+++ form.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<html>
+ <head>
+  <meta charset="utf-8">
+   <title>Succes!</title>
+  </head>
+  <body bgcolor="lightblue">
+  	<h2><font color="blue">Ваша форма успешна отправлена!</font></h2>
+  	<p><a href="/pepka">отправить форму ещё раз!</a></p>
+  </body>
+  </html>
